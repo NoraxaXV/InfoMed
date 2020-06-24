85ceec5193f40b486f055685575f20b16ce4307a
Aaron Smith <magik_smith@outlook.com>
2020-06-24 15:52:53 -0400

    fix style.css to properly load

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('www/index.html'));
-});
+app.use(express.static('www'));
 
 app.listen(3000, () => {
-  console.log('server started');
+    console.log('server started, dirname=' + __dirname);
 });
