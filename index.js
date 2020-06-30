c5a36ad0bb69809a1db62e2d1afade0a94c51d36
RebornMaster <59583959+RebornMaster@users.noreply.github.com>
2020-06-29 21:02:16 -0400

    Finished search bar, working on rendering search page

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const path = require('path');
+const fs = require('fs');
+const Vue = require('vue');
+const renderer = require('vue-server-renderer').createRenderer();
+
 
 const app = express();
 
-app.use(express.static('www'));
-app.use("/explore", express.static("www/explore"));
+app.use("/", express.static('www'));
+
+app.get("/explore/:key", function(req, res){
+    
+    var app = new Vue({
+        data: require("./database.json"),
+        template: fs.readFileSync('./pages/search.html')
+    });
+
+    renderer.renderToString(app, function(err, html){
+        console.log(html);
+        if (err) {
+            res.status(500).end('Internal Server Error');
+            console.log(err);
+            return;
+        }
+        res.send(html);
+    });
+
+    res.send("you sent the follwing key:" + req.params.key);
+});
 
 app.listen(3000, () => {
     console.log('server started, dirname=' + __dirname);
