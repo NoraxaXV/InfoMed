38c97c6a361af71a5b3acb73937eac1b77d4da92
RebornMaster <59583959+RebornMaster@users.noreply.github.com>
2020-06-30 20:21:24 -0400

    Got Server Rendered Search Page

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const fs = require('fs');
-const Vue = require('vue');
-const renderer = require('vue-server-renderer').createRenderer();
+const express = require("express");
+const fs = require("fs");
+const Vue = require("vue");
 
+const renderer = require("vue-server-renderer").createRenderer({
+	template: require("fs").readFileSync("./pages/index.template.html", "utf-8")
+});
 
 const app = express();
 
-app.use("/", express.static('www'));
+app.use("/", express.static("www"));
 
-app.get("/explore/:key", function(req, res){
+app.get("/explore/:key", function(req, res) {
+	let database = require("./database.json");
     
     var app = new Vue({
-        data: require("./database.json"),
-        template: fs.readFileSync('./pages/search.html')
-    });
+        data: {database: database},
+        template: fs.readFileSync("./pages/search.html", "utf8")
+	});
 
-    renderer.renderToString(app, function(err, html){
-        console.log(html);
-        if (err) {
-            res.status(500).end('Internal Server Error');
-            console.log(err);
-            return;
-        }
-        res.send(html);
-    });
+	renderer.renderToString(
+		app,
+		{ title: "InfoMed: Get the Info On " + req.params.key },
+		function(err, html) {
 
-    res.send("you sent the follwing key:" + req.params.key);
+			if (err) {
+				res.status(500).end("Internal Server Error");
+				console.log(`ERROR RENDERING VUE JS: \n  ${err}`);
+				return;
+			}
+
+			res.send(html);
+		}
+	);
 });
 
 app.listen(3000, () => {
-    console.log('server started, dirname=' + __dirname);
+	console.log("server started, dirname=" + __dirname);
 });
-
-
